4718a751f0813bfc7f722e1ca6f6be699e554789
George Semenov <me@gsemenov.com>
2012-07-16 14:33:42 -0500

    Game spec refactoring.

--- spec/codebreaker/game_spec.rb
+++ spec/codebreaker/game_spec.rb
@@ -6,35 +6,32 @@ module Codebreaker
     let(:game)   { Game.new(output) }
 
     describe "#start" do
+      after(:each) { game.start('1234') }
+
       it "sends a welcome messgae" do
         output.should_receive(:puts).with("Welcome to Codebreaker! [Enter 'g' to give up]")
-        game.start('1234')
       end
 
       it "prompts for the first guess" do
         output.should_receive(:puts).with('Enter guess:')
-        game.start('1234')
       end
     end
 
     describe "#guess" do
+      before(:each) { game.start('1234') }
+      after(:each)  { game.guess('1234') }
+
       it "sends a mark to output" do
-        game.start('1234')
         output.should_receive(:puts).with('++++')
-        game.guess('1234')
       end
 
       context "wining condition" do
         it "sends a wining message" do
-          game.start('1234')
           output.should_receive(:puts).with("Congratulations! You're Winner!")
-          game.guess('1234')
         end
 
         it "sends a request for next round" do
-          game.start('1234')
           output.should_receive(:puts).with("Another round? [y/n]")
-          game.guess('1234')
         end
       end
     end
